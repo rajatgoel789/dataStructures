8042f4119ee8a9a30443f9e99f0afc0cc39e1c43
Rajat Goel <rajatgoel@outlook.in>
2023-06-13 22:10:06 +0530

    leetcode blind75 problems

---
+++ leetcode/encode_decode_string.js
@@ -0,0 +1,11 @@
+// Leetcode 271
+
+var encode = function(strs){
+    if(!s.length) return null;
+    return strs.join("-encodeStr")
+}
+
+var decode = function(s){
+    if(!s) return [];
+    return s.split("-encodeStr");   
+}
